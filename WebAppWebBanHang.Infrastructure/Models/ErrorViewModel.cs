24335bf0ec4c198a00f116f7864eaec0340d9256
bachhop9880 <bachhop9880@gmail.com>
2019-04-05 09:23:46 +0700

    lession7:create solution

---
+++ WebAppWebBanHang.Infrastructure/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppWebBanHang.Infrastructure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
